0654c3d3282cd584330d9d77894c939545ef3081
Ibrahima <ibracool99@gmail.com>
2022-01-11 02:23:55 +0100

    display columns

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import './App.css';
 import BootstrapTable from 'react-bootstrap-table-next';
+import {useEffect, useState} from "react";
 function App() {
-    const data=[{
-        "id":1,
-        "name":"jj",
-        "price":4
-    }]
-    const columns = [{
-        dataField: 'id',
-        text: 'Product ID'
-    }, {
-        dataField: 'name',
-        text: 'Product Name'
+
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((json) => setProducts(json));
+    }, []);
+
+    const imageFormatter = (cell, row) =>{
+                return (
+        <img src={cell}
+             title={row.title}
+        width={100}></img>
+                );
+            }
+    const ratingFormatter = (cell, row) =>{
+        let color='green';
+        if (cell>=3 && cell<4){
+            color='orange'
+        }else if (cell<3){
+            color ='red'
+        }
+        return (  <span><strong style={ { color: color } }> { cell }</strong></span>
+
+        );
+    }
+    const columns = [
+        {
+            dataField: 'image',
+            formatter: imageFormatter
+        },
+        {
+        dataField: 'title',
+        text: 'Title'
     }, {
         dataField: 'price',
-        text: 'Product Price'
-    }];
+        text: 'Price'
+    }, {
+        dataField: 'category',
+        text: 'Category'
+    },
+        {
+            dataField: 'description',
+            text: 'Description'
+        },
+        {
+            dataField: 'rating.rate',
+            text: 'Rating',
+            formatter: ratingFormatter
+        }];
   return (
     <div className="App">
       <h1>Products list</h1>
-        <BootstrapTable keyField='id' data={data} columns={ columns } />
+        <BootstrapTable keyField='id' data={products} columns={ columns } />
     </div>
   );
 }
